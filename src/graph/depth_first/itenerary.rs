89d030c317d5364945f06b272682e19c2939d661
Brian Heise <92563897+bnheise@users.noreply.github.com>
2022-04-12 06:53:25 +0900

    started next chalenge

--- src/graph/depth_first/itenerary.rs
+++ src/graph/depth_first/itenerary.rs
@@ -1,10 +1,37 @@
+use std::collections::{hash_map::Entry, HashMap};
+
 struct Solution {}
 
 #[allow(dead_code)]
 impl Solution {
     pub fn find_itinerary(tickets: Vec<Vec<String>>) -> Vec<String> {
+        let mut edge_list: HashMap<&str, Vec<&str>> =
+            tickets.iter().fold(HashMap::new(), build_edgelist);
+        edge_list
+            .values_mut()
+            .map(|edges| {
+                edges.sort_by(|a, b| b.cmp(a));
+                edges
+            })
+            .collect();
+        println!("{:?}", edge_list);
         unimplemented!()
     }
+}
+
+fn build_edgelist<'a>(
+    mut map: HashMap<&'a str, Vec<&'a str>>,
+    edge: &'a Vec<String>,
+) -> HashMap<&'a str, Vec<&'a str>> {
+    match map.entry(&edge[0]) {
+        Entry::Occupied(mut entry) => {
+            entry.get_mut().push(&edge[1]);
+        }
+        Entry::Vacant(entry) => {
+            entry.insert(vec![&edge[1]]);
+        }
+    };
+    map
 }
 
 #[cfg(test)]
@@ -20,7 +47,7 @@ mod tests {
             ["LHR", "SFO"],
         ]
         .iter()
-        .map(|&edge| vec![edge.iter().map(|&str| String::from(str)).collect()])
+        .map(|&edge| edge.iter().map(|&str| String::from(str)).collect())
         .collect();
 
         let output: Vec<String> = vec!["JFK", "MUC", "LHR", "SFO", "SJC"]
@@ -43,7 +70,7 @@ mod tests {
             ["ATL", "SFO"],
         ]
         .iter()
-        .map(|&edge| vec![edge.iter().map(|&str| String::from(str)).collect()])
+        .map(|&edge| edge.iter().map(|&str| String::from(str)).collect())
         .collect();
 
         let output: Vec<String> = vec!["JFK", "ATL", "JFK", "SFO", "ATL", "SFO"]
