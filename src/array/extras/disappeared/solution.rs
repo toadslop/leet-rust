77bf2ab74010f16b62eec0348a9d38e3ad2113df
Brian Heise <92563897+bnheise@users.noreply.github.com>
2022-05-27 19:54:40 +0900

    finished disappeared

---
+++ src/array/extras/disappeared/solution.rs
@@ -0,0 +1,32 @@
+use std::collections::HashSet;
+
+#[allow(dead_code)]
+impl Solution {
+  pub fn find_disappeared_numbers(nums: Vec<i32>) -> Vec<i32> {
+    let vec: Vec<i32> = (1..=nums.len() as i32).collect();
+    let mut result: HashSet<i32> = vec.into();
+    for num in nums.iter() {
+      result.remove(num);
+    }
+    result.into_iter().collect()
+  }
+}
+
+struct Solution {}
+
+#[cfg(test)]
+mod test {
+    use super::Solution;
+
+    #[test]
+    fn example_1() {
+        let nums = vec![4,3,2,7,8,2,3,1];
+
+        let expected_output = vec![5,6];
+        let actual_output = Solution::find_disappeared_numbers(nums);
+
+        assert_eq!(expected_output, actual_output)
+    }
+
+    
+}
--- src/array/extras/mod.rs
+++ src/array/extras/mod.rs
@@ -1,3 +1,4 @@
 mod consecutive_ones;
 mod height_checker;
 mod third_max;
+mod disappeared;
